9216ba6518b509aab3f83558bce9c4e8cbc05a95
Dibyajyoti Dhar <dibyajyoti.dhar@anchanto.com>
2021-02-15 02:24:29 +0530

    heroku deploy try

--- db/migrate/20210212053140_friendship.rb
+++ db/migrate/20210212053140_friendship.rb
@@ -1,4 +1,4 @@
-class FriendshipCreate < ActiveRecord::Migration[6.1]
+class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
     create_table :friendships do |t|
       t.integer :sender_id
